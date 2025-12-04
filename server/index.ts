f29348c02e1affca006509b7cc5ad7f133724ca4
anand <a@google.com>
2025-12-04 10:23:51 +0530

    Enable CORS

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
+import cors from "cors";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
@@ -17,6 +18,7 @@ app.use(express.json({
     req.rawBody = buf;
   }
 }));
+app.use(cors());
 app.use(express.urlencoded({ extended: false, limit: '5mb' }));
 
 
